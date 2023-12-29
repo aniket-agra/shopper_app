5e389ab625ecc66b4d7a1618e474c01e2a22c81e
Aniket Agrawal <aniket.mpa@gmail.com>
2023-12-29 08:04:26 +0100

    Add product cards to product list component

--- frontend/components/ProductList.jsx
+++ frontend/components/ProductList.jsx
@@ -1,15 +1,27 @@
 import axios from "axios";
 import { ProductCard } from "./ProductCard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function ProductList() {
+    const [products, setProducts] = useState([]);
+
     useEffect(
         function () {
             axios
                 .get("http://localhost:3001/api/products")
-                .then(data => console.log(data));
+                .then(response => setProducts(response.data));
         }, []
     );
+
+    return (
+        <div>
+            {
+                products.map(product => <li key = {product.id}>
+                    <ProductCard productData = {product} addToCart = { () => console.log(product)} />
+                </li>)
+            }
+        </div>
+    )
 }
 
 export { ProductList };
