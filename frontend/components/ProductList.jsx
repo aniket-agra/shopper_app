f14b1f0fa395327d3b191ff0e9347f673e565873
Aniket Agrawal <aniket.mpa@gmail.com>
2023-12-29 07:58:34 +0100

    Create product list component

--- frontend/components/ProductList.jsx
+++ frontend/components/ProductList.jsx
@@ -1,5 +1,15 @@
+import axios from "axios";
+import { ProductCard } from "./ProductCard";
+import { useEffect } from "react";
+
 function ProductList() {
-    
+    useEffect(
+        function () {
+            axios
+                .get("http://localhost:3001/api/products")
+                .then(data => console.log(data));
+        }, []
+    );
 }
 
 export { ProductList };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import { ShoppingCart } from "../components/ShoppingCart";
+import { ProductList } from "../components/ProductList";
+
+let data = [
+  {
+    id : 1,
+    title : "Lectures on Physics",
+    details : "Richard Feynman talks about classical physics",
+    price: "100$"
+  },
+  {
+    id : 2,
+    title : "Relativity: The special and general theory",
+    details : "Albert Einstein talks about relativity",
+    price : "20$"
+  },
+  {
+    id : 3,
+    title : "The large-scale structure of spacetime",
+    details : "Hawking and Penrose talk about spacetime",
+    price : "50$"
+  }
+];
 
 function App () {
   return (
     <>
-      <ShoppingCart data = {data} />
+      {/* <ShoppingCart data = {data} /> */}
+      <ProductList />
     </>
   )
 }
