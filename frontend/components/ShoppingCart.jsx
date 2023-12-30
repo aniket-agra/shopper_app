e70aebfbc7144acd14338abb364f009798a175a3
Aniket Agrawal <aniket.mpa@gmail.com>
2023-12-30 08:37:47 +0100

    Display title, price, quantity, and cart total

--- frontend/components/ShoppingCart.jsx
+++ frontend/components/ShoppingCart.jsx
@@ -6,6 +6,7 @@ function CartItemCard( { cartItem }) {
   return (
     <>
       <p>Product Name: {cartItem.title}</p>
+      <p>Product Price: {cartItem.price}</p>
       <p>Quantity Ordered: {cartItem.quantity}</p>
     </>
   );  
@@ -29,6 +30,15 @@ function ShoppingCart({ changeView }) {
             return <CartItemCard key = {item.id} cartItem = { item } />
           })
         }
+      <p>
+        Cart Total: {
+          cartItems.reduce((accumulator, currentElement) => {
+            let itemPrice = currentElement.price;
+            itemPrice = Number(itemPrice.substring(0, itemPrice.length - 1));
+            return accumulator + (itemPrice * Number(currentElement.quantity));
+          }, 0)
+        }
+      </p>
       <button onClick = { changeView }>View Products</button>
     </>
   )  
