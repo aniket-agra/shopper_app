65322cf8d9d4a67426e21412b423501303f49133
Aniket Agrawal <aniket.mpa@gmail.com>
2023-12-30 07:41:08 +0100

    Remove data variable from App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,27 +2,6 @@ import { ShoppingCart } from "../components/ShoppingCart";
 import { ProductList } from "../components/ProductList";
 import { useState } from "react";
 
-let data = [
-  {
-    id : 1,
-    title : "Lectures on Physics",
-    details : "Richard Feynman talks about classical physics",
-    price: "100$"
-  },
-  {
-    id : 2,
-    title : "Relativity: The special and general theory",
-    details : "Albert Einstein talks about relativity",
-    price : "20$"
-  },
-  {
-    id : 3,
-    title : "The large-scale structure of spacetime",
-    details : "Hawking and Penrose talk about spacetime",
-    price : "50$"
-  }
-];
-
 function App () {
 
   const [viewList, setViewList] = useState(true);
