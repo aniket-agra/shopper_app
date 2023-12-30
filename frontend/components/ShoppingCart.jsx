c374fae1b282b4a01faf42ae48ee3b63b025d940
Aniket Agrawal <aniket.mpa@gmail.com>
2023-12-30 08:02:16 +0100

    Display cart contents in cart component

--- frontend/components/ShoppingCart.jsx
+++ frontend/components/ShoppingCart.jsx
@@ -1,12 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function ShoppingCart({ changeView }) {
-    
+
+  const [cartItems, setCartItems] = useState([]);
+  useEffect(function () {
+    axios
+      .get("http://localhost:3001/api/cart")
+      .then(response => {
+        setCartItems(response.data);
+      });
+  }, []);
+
+  if (cartItems.length > 0) {
+    return (
+      <>
+          {
+            cartItems.map(item => {
+              return <li key = {item.id}>Item ID: {item.id} Item Quantity: {item.quantity}</li>
+            })
+          }
+        <button onClick = { changeView }>View Products</button>
+      </>
+    )
+  }
+  else {
     return (
       <>
         <button onClick = { changeView }>View Products</button>
       </>
     )
+  }
+  
 }
 
 export {ShoppingCart}
